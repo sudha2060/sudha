4ebda0e4772b43a724b1a7cda6a0b25224ac5d30
sudha thapa magar <sudhathapamagar08@gmail.com>
2025-02-20 08:05:29 +0545

    first comment

---
+++ comment.py
@@ -0,0 +1,4 @@
+#this is addition of two number
+a=5
+b=6
+print(a+b)
---
+++ varaible.py
@@ -0,0 +1,45 @@
+# y="test"
+# def myfunc():
+#     y="this is not a test"
+#     print(y)
+# myfunc()
+# print(y)
+
+# x= 6
+# b=float(x)
+# print(b)
+
+# x=6
+# print(float(x))
+
+#slicing
+# txt="hello Bit Class"
+# print(txt[4:8])
+
+# txt="hello, world"
+# print(txt[-5:-2])
+
+# x="UAT_Demo_Domain"
+# print(x[4:8])
+# print(x[7:12])
+# print(x[::-1])
+
+#wAP to take two inputs and print sum, difference, multiplication division
+
+
+a=int(input("enter the first number:"))
+b=int(input("enter the second number:"))
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+
+# odd even
+num = int(input("Enter a number: "))
+if num % 2 == 0:
+    print(" The number is Even")
+else:
+    print("The number is Odd")
+
+
+
